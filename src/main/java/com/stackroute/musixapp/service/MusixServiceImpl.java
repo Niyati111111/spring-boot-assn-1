b5182978224749d0ae5102833dd71f9f51dd4191
niymish <niymish1@in.ibm.com>
2019-07-24 17:17:01 +0530

    eigth task

--- src/main/java/com/stackroute/musixapp/service/MusixServiceImpl.java
+++ src/main/java/com/stackroute/musixapp/service/MusixServiceImpl.java
@@ -5,6 +5,7 @@ import com.stackroute.musixapp.exceptions.TrackAlreadyExistsException;
 import com.stackroute.musixapp.exceptions.TrackNotFoundException;
 import com.stackroute.musixapp.repository.MusixRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -15,6 +16,19 @@ import java.util.Optional;
 
 @Service
 public class MusixServiceImpl implements MusixService, ApplicationListener<ContextRefreshedEvent>, CommandLineRunner {
+
+    @Value("${musix.1.name:default}")
+    String name1;
+    @Value("${musix.1.rating:default}")
+    int rating1;
+    @Value("${musix.1.comments:default}")
+    String comments1;
+    @Value("${musix.2.name:default}")
+    String name2;
+    @Value("${musix.2.rating:default}")
+    int rating2;
+    @Value("${musix.2.comments:default}")
+    String comments2;
 
     MusixRepository musixRepository;
 
@@ -80,8 +94,8 @@ public class MusixServiceImpl implements MusixService, ApplicationListener<Conte
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-        musixRepository.save(new Musix(1, "all of me", 5, "wow"));
-        musixRepository.save(new Musix(2, "DNA", 5, "wow"));
+        musixRepository.save(new Musix(1, name1, rating1, comments1));
+        musixRepository.save(new Musix(2, name2, rating2, comments2));
     }
 
     @Override
